0277dc8dba2b88c89e8e47347a7589b66affea38
Rohan Ghosh <156415986+a-AvgLearner@users.noreply.github.com>
2024-07-14 17:51:27 +0000

    Day 2 DONE

---
+++ Day 2.js
@@ -0,0 +1,15 @@
+const a= 80;
+const b= 5;
+
+let c= a+b;
+console.log(`${c}`);
+console.log('Subtract'+" "+a-b);
+console.log('Mutiplcation' + " "+a*b);
+console.log("divide" +" "+ a/b);
+console.log('remainder'+" "+a%b);
+
+c+=1;
+console.log(c);
+
+c-=1;
+console.log(c);
