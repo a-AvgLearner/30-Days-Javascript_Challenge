fb9cc4b4f42c5a070c9ac2fd4ff1609797239d3e
Rohan Ghosh <156415986+a-AvgLearner@users.noreply.github.com>
2024-07-14 17:26:49 +0000

    DAY 1 done

---
+++ Day 1.js
@@ -0,0 +1,28 @@
+var a = 5;
+
+let b= 'lol';
+
+console.log(a);
+console.log(b);
+
+const boy= true;
+
+console.log(boy);
+
+const string = 'lool';
+const num= 45;
+const  bool= true;
+const obj ={name: "john" , age :30}
+const arr= [1,2,3];
+
+
+console.log( typeof string +" "+ typeof num+ " "+ typeof bool+ " "+ typeof obj+" "+ typeof arr);
+
+let you = 'duke';
+// let you = 'luke';
+
+console.log (you);
+
+const u= 45;
+const u= '64';
+console.log(u);
