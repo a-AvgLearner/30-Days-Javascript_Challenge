bf3095a24f879de99f12f0000275fb1c35bdabe1
Rohan Ghosh <156415986+a-AvgLearner@users.noreply.github.com>
2024-07-23 17:35:28 +0000

    Day 4 Done

---
+++ Day 4.js
@@ -0,0 +1,22 @@
+//For loop
+
+for(let i=1;i<=10;i++){
+    console.log(i)
+}
+
+//multiplication table
+
+for(let i=1; i<=10;i++){
+    // console.log(i*5)
+}
+
+
+//Do -while
+
+let sum=0;
+let j=0;
+while( j <= 10){
+     sum += j;
+     j++
+     console.log(sum)
+}
